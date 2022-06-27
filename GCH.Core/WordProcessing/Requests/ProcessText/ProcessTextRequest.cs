6898ca9e8bf9855036df446b898a2784b6577aea
bohdan.kovalevych <bohdan.kovalevych@nure.ua>
2022-06-27 15:14:09 +0300

    Add project files.

---
+++ GCH.Core/WordProcessing/Requests/ProcessText/ProcessTextRequest.cs
@@ -0,0 +1,14 @@
+﻿using LanguageExt.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCH.Core.WordProcessing.Requests.ProcessText
+{
+    public class ProcessTextRequest : IRequest<Result<string>>
+    {
+
+    }
+}
